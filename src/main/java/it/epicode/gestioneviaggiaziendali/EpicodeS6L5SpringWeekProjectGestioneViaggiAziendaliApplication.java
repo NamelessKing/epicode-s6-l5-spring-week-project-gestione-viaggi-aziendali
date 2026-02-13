b040d468f1a4f9c9c77aa73b9f0800201bc4dfd8
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-13 12:40:27 +0100

    first commit

---
+++ src/main/java/it/epicode/gestioneviaggiaziendali/EpicodeS6L5SpringWeekProjectGestioneViaggiAziendaliApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.gestioneviaggiaziendali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeS6L5SpringWeekProjectGestioneViaggiAziendaliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicodeS6L5SpringWeekProjectGestioneViaggiAziendaliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/epicode/gestioneviaggiaziendali/EpicodeS6L5SpringWeekProjectGestioneViaggiAziendaliApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.gestioneviaggiaziendali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeS6L5SpringWeekProjectGestioneViaggiAziendaliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
